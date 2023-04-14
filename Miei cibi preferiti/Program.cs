f615f06698373a4e0cca05fc281bfe957ed346f0
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-14 14:08:55 +0200

    Add project files.

---
+++ Miei cibi preferiti/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System.Collections;
+
+string[] favoritFood = { "pomodori all'insalata", "carbonara", "patate al forno", "focaccia barese", "sushi", "patatine fritte con cheddar" };
+
+    Console.WriteLine("i miei cibi preferiti in totale sono: " +  favoritFood.Length);
+
+
+    Console.WriteLine("------------------------------------------------------------");
+
+ArrayList(favoritFood);
+
+
+
+
+
+void ArrayList(string[] array)
+{
+   for(int i  = 1; i < array.Length; i++)
+    {
+        Console.WriteLine((i) + " " + array[i]);
+    }
+
+   
+}
+
